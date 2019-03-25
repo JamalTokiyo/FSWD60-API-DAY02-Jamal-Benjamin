c9712adeb862685907d2fa805c912e8c4a0e17fe
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-03-25 10:29:24 +0100

    add content and serri jokes

--- benjamin codereview 2/index.php
+++ benjamin codereview 2/index.php
@@ -5,6 +5,12 @@
 	<title>Second CodeReview</title>
 	<link rel="stylesheet" type="text/css" href="Second_CodeReview_Style.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<!-- This is the content of the whole page -->
@@ -27,6 +33,49 @@
 				<li><a href="video.html" title="Video">Video</a></li>
 			</ul>
 		</nav>
+		<div class="container">
+			<div style="text-align: center" class="jokes">
+					<?php
+
+						require_once 'RESTful.php';
+						$url = 'http://api.serri.codefactory.live/random/';
+						$xml = file_get_contents($url);
+						echo $xml;
+
+					?>	
+			</div>
+			<hr>
+			<div class="row">	
+				<div class="col-lg-8 bbc">
+					<h1>BBC API</h1>
+					<?php
+
+						require_once 'RESTful.php';
+						$url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
+						$response = curl_get($url);
+						$xml = simplexml_load_string($response);
+						foreach ($xml->channel->item as $item) {
+						echo '<a href="'.$item->link.'" target="_blank">'.$item->title.'</a><br>';
+						}
+
+					?>
+				</div>
+				<div class="col-lg-4 cnn">
+					<h1>CNN API</h1>
+					<?php
+
+						require_once 'RESTful.php';
+						$url = 'http://rss.cnn.com/rss/edition_technology.rss';
+						$response = curl_get($url);
+						$xml = simplexml_load_string($response);
+						foreach ($xml->channel->item as $item) {
+						echo '<a href="'.$item->link.'" target="_blank">'.$item->title.'</a><br>';
+						}
+
+					?>
+				</div>
+			</div>				
+		</div>
 		<!-- This is the footer at the bottom of the page -->
 		<div class="footer">
 			<ul class="ulist down">
