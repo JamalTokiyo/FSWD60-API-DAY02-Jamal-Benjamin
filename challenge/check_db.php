ca456c25415f8135eee7bf43a3127945077969f6
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-03-25 14:48:41 +0100

    Add file

--- challenge/check_db.php
+++ challenge/check_db.php
@@ -5,7 +5,7 @@
 
 	// This query will check do we have car_id in the table car
 	// for the provided $id
-	$sql ="SELECT COUNT(media.media_id) FROM media WHERE mediaStatus = 1";
+	$sql ="SELECT COUNT(media.media_id) as total FROM media WHERE mediaStatus = 1";
 
 	// Perform a query on the DB
 	$result = mysqli_query($conn, $sql);
@@ -13,8 +13,7 @@
 	// Fetch the query into $row
 	$row = mysqli_fetch_assoc($result);
 
-	// Store values into the variables
-	$status = $row['mediaStatus'];
+	echo "The book library currently has ". $row['total']. " books available.";
 
 	// Close the DB connection
 	mysqli_close($conn);
